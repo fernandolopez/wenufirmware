e97d64d6ed4e63118ae63a698b8b62d36cf295cb
Emilio Crudele <ercrude@gmail.com>
2016-11-21 15:30:27 -0300

    Un par de unit test añadidos

--- contiki/clima-mqtt/unit-tests.c
+++ contiki/clima-mqtt/unit-tests.c
@@ -4,6 +4,14 @@
 
 #include "mqtt-sensores.h"
 
+#define STRING_PRUEBA "string_prueba"
+#define TEMP_PRUEBA 28.13
+#define TEMP_INT_PRUEBA 28
+#define DEC_PRUEBA 13
+#define CURR_PRUEBA 3000
+#define MOV_PRUEBA 0
+#define VOLT_PRUEBA 2000
+
 // /*---------------------------------------------------------------------------*/
 
 // UNIT_TEST_REGISTER(temp_rango, "Verificar si la temperatura leida esta en un rango razonable");
@@ -14,18 +22,6 @@
 // 	// rango de temperatura: -40º - 123.8º
 // 	// http://zolertia.sourceforge.net/wiki/index.php/Z1_Sensors#ZIG001_Temperature-Humidity_Sensor
 // 	UNIT_TEST_ASSERT(temperatura >= -40 && temperatura <= 124);
-
-// 	UNIT_TEST_END();
-// }
-
-// /*---------------------------------------------------------------------------*/
-
-// UNIT_TEST_REGISTER(temp_digitos, "Verificar si la parte entera de la temperatura son dos digitos");
-
-// UNIT_TEST(temp_digitos) {
-// 	UNIT_TEST_BEGIN();
-
-// 	UNIT_TEST_ASSERT(temperatura == temperatura % 100);
 
 // 	UNIT_TEST_END();
 // }
@@ -58,14 +54,7 @@
 
 /*---------------------------------------------------------------------------*/
 
-#define STRING_PRUEBA "string_prueba"
-// #define TEMP_PRUEBA 2
-// #define DEC_PRUEBA 3
-// #define CURR_PRUEBA 4
-// #define MOV_PRUEBA 0
-// #define VOLT_PRUEBA 1
-
-UNIT_TEST_REGISTER(format_message_correcto, "Verificar si la funcion format_message funciona bien");
+UNIT_TEST_REGISTER(format_message_correcto, "Verificar si format_message funciona bien");
 
 UNIT_TEST(format_message_correcto) {
 	UNIT_TEST_BEGIN();
@@ -80,6 +69,49 @@ UNIT_TEST(format_message_correcto) {
 							"}";
 	// strcmp() devuelve 0 si los dos strings son equivalentes
 	UNIT_TEST_ASSERT(strcmp(stringFormatMessage, stringUnitTest) == 0);
+
+	UNIT_TEST_END();
+}
+
+/*---------------------------------------------------------------------------*/
+
+UNIT_TEST_REGISTER(temperatura_split_correcto, "Verificar si temperatura_split devuelve valores razonables");
+
+UNIT_TEST(temperatura_split_correcto) {
+	UNIT_TEST_BEGIN();
+
+	int16_t temperatura_test = TEMP_PRUEBA;
+	int16_t tempint_test;
+	uint16_t decimas_test;
+	temperature_split(temperatura_test, &tempint_test, &decimas_test);
+
+	UNIT_TEST_ASSERT(decimas_test == temperatura_test % 100);
+	// verifica si devuelve dos digitos
+	UNIT_TEST_ASSERT(tempint_test == tempint_test % 100);
+
+	UNIT_TEST_END();
+}
+
+/*---------------------------------------------------------------------------*/
+
+UNIT_TEST_REGISTER(validate_correcto, "Verificar si validate funciona bien");
+
+UNIT_TEST(validate_correcto) {
+	UNIT_TEST_BEGIN();
+
+	// todo correcto
+	UNIT_TEST_ASSERT(validate(TEMP_INT_PRUEBA, DEC_PRUEBA, CURR_PRUEBA, VOLT_PRUEBA, MOV_PRUEBA) == 1);
+	// temperaturas invalidas
+	UNIT_TEST_ASSERT(validate(-60, DEC_PRUEBA, CURR_PRUEBA, VOLT_PRUEBA, MOV_PRUEBA) == 0);
+	UNIT_TEST_ASSERT(validate(140, DEC_PRUEBA, CURR_PRUEBA, VOLT_PRUEBA, MOV_PRUEBA) == 0);
+	// decimas invalidas
+	UNIT_TEST_ASSERT(validate(TEMP_INT_PRUEBA, 150, CURR_PRUEBA, VOLT_PRUEBA, MOV_PRUEBA) == 0);
+	// corriente invalida
+	UNIT_TEST_ASSERT(validate(TEMP_INT_PRUEBA, DEC_PRUEBA, 5000, VOLT_PRUEBA, MOV_PRUEBA) == 0);
+	// voltaje invalido
+	UNIT_TEST_ASSERT(validate(TEMP_INT_PRUEBA, DEC_PRUEBA, CURR_PRUEBA, 5000, MOV_PRUEBA) == 0);
+	// movimiento invalido
+	UNIT_TEST_ASSERT(validate(TEMP_INT_PRUEBA, DEC_PRUEBA, CURR_PRUEBA, VOLT_PRUEBA, 5) == 0);
 
 	UNIT_TEST_END();
 }
